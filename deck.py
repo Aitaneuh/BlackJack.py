d0d58faa4b2ebc8cab7e68b15f6dfab7958405ca
Aitaneuh <ethanb9407@gmail.com>
2025-08-22 15:08:23 +0200

    added deck class

---
+++ deck.py
@@ -0,0 +1,12 @@
+from card import Card
+import random
+
+class Deck:
+    def __init__(self):
+        self.cards = [Card(v,s) for v in range(1,14) for s in ['Hearts','Spades','Diamonds','Clubs']]
+
+    def shuffle(self) -> None:
+        random.shuffle(self.cards)
+
+    def draw(self) -> Card:
+        return self.cards.pop()
