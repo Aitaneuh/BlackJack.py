ee6a6153254fc94ceaf2cd36a1b3ccc95f3e5bcb
Aitaneuh <ethanb9407@gmail.com>
2025-08-25 11:31:04 +0200

    changed the way the cards are made

--- deck.py
+++ deck.py
@@ -3,7 +3,7 @@ import random
 
 class Deck:
     def __init__(self) -> None:
-        self.cards = [Card(v,s) for v in range(1,14) for s in ['Hearts','Spades','Diamonds','Clubs']]
+        self.cards = [Card(v,s) for v in [2,3,4,5,6,7,8,9,10,"Jack","Queen","King","Ace"] for s in ['Hearts','Spades','Diamonds','Clubs']]
 
     def shuffle(self) -> None:
         random.shuffle(self.cards)
