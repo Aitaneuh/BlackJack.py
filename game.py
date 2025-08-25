058aab8dfeec7438c6bc6b2329eee26baf28421a
Aitaneuh <ethanb9407@gmail.com>
2025-08-25 11:58:45 +0200

    some small corrections

--- game.py
+++ game.py
@@ -15,7 +15,7 @@ class Game:
         return False
 
 
-    def play_round(self):
+    def play_one_game(self):
         self.player.add_card(self.deck.draw())
         self.player.add_card(self.deck.draw())
 
@@ -27,11 +27,11 @@ class Game:
         dealer_blackjack = self.has_blackjack(self.dealer)
 
         if player_blackjack and not dealer_blackjack:
-            return {"Outcome":"blackjack", "player_score":21, "dealer_score":self.dealer.get_hand_value, "player_hit_count":0, "dealer_hit_count":0, "score":1}
+            return {"outcome":"blackjack", "player_score":21, "dealer_score":self.dealer.get_hand_value(self.dealer.hand), "player_hit_count":0, "dealer_hit_count":0, "score":1}
         elif player_blackjack and dealer_blackjack:
-            return {"Outcome":"draw", "player_score":21, "dealer_score":21, "player_hit_count":0, "dealer_hit_count":0}
+            return {"outcome":"draw", "player_score":21, "dealer_score":21, "player_hit_count":0, "dealer_hit_count":0}
         elif not player_blackjack and dealer_blackjack:
-            return {"Outcome":"dealer_blackjack", "player_score":self.player.get_hand_value(self.player.hand), "dealer_score":21, "player_hit_count":0, "dealer_hit_count":0, "score":-1}
+            return {"outcome":"dealer_blackjack", "player_score":self.player.get_hand_value(self.player.hand), "dealer_score":21, "player_hit_count":0, "dealer_hit_count":0, "score":-1}
 
         player_hit_count = 0
         while True:
@@ -42,7 +42,7 @@ class Game:
                 self.player.add_card(self.deck.draw())
 
             if self.player.get_hand_value(self.player.hand) > 21:
-                return {"Outcome":"bust", "player_score":self.player.get_hand_value(self.player.hand), "dealer_score":self.dealer.get_hand_value(self.dealer.hand), "player_hit_count":player_hit_count, "dealer_hit_count":0, "score":-1}
+                return {"outcome":"bust", "player_score":self.player.get_hand_value(self.player.hand), "dealer_score":self.dealer.get_hand_value(self.dealer.hand), "player_hit_count":player_hit_count, "dealer_hit_count":0, "score":-1}
 
         dealer_hit_count = 0
         while True:
@@ -53,14 +53,14 @@ class Game:
                 self.dealer.add_card(self.deck.draw())
             
             if self.dealer.get_hand_value(self.dealer.hand) > 21:
-                return {"Outcome":"dealer_bust", "player_score":self.player.get_hand_value(self.player.hand), "dealer_score":self.dealer.get_hand_value(self.dealer.hand), "player_hit_count":player_hit_count, "dealer_hit_count":dealer_hit_count, "score":1}
+                return {"outcome":"dealer_bust", "player_score":self.player.get_hand_value(self.player.hand), "dealer_score":self.dealer.get_hand_value(self.dealer.hand), "player_hit_count":player_hit_count, "dealer_hit_count":dealer_hit_count, "score":1}
             
         player_hand_value = self.player.get_hand_value(self.player.hand)
         dealer_hand_value = self.dealer.get_hand_value(self.dealer.hand)
 
         if player_hand_value > dealer_hand_value:
-            return {"Outcome":"win", "player_score":player_hand_value, "dealer_score":dealer_hand_value, "player_hit_count":player_hit_count, "dealer_hit_count":dealer_hit_count, "score":1}
+            return {"outcome":"win", "player_score":player_hand_value, "dealer_score":dealer_hand_value, "player_hit_count":player_hit_count, "dealer_hit_count":dealer_hit_count, "score":1}
         elif player_hand_value == dealer_hand_value:
-            return {"Outcome":"draw", "player_score":player_hand_value, "dealer_score":dealer_hand_value, "player_hit_count":player_hit_count, "dealer_hit_count":dealer_hit_count, "score":0}
+            return {"outcome":"draw", "player_score":player_hand_value, "dealer_score":dealer_hand_value, "player_hit_count":player_hit_count, "dealer_hit_count":dealer_hit_count, "score":0}
         else:
-            return {"Outcome":"lose", "player_score":player_hand_value, "dealer_score":dealer_hand_value, "player_hit_count":player_hit_count, "dealer_hit_count":dealer_hit_count, "score":-1}
+            return {"outcome":"lose", "player_score":player_hand_value, "dealer_score":dealer_hand_value, "player_hit_count":player_hit_count, "dealer_hit_count":dealer_hit_count, "score":-1}
